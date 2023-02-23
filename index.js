757604c12361840a445b0d6f60f19f2fc860704b
juan75amieva <juan75amieva@gmail.com>
2023-02-23 21:02:54 +0100

    feat: add server

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const { conexion } = require('./baseDeDatos/conexion')
+const express = require("express")
+const cors = require('cors')
 
+
+//iniciar la app
 console.log("App de node iniciada")
 
 
+
 conexion()
+
+//crear el servidor
+const app = express()
+const puerto = 5000
+
+//configurar el cors
+app.use(cors())
+
+//convertir el body a un objeto js
+
+app.use(express.json())
+
+//crear el servidor y escuchar peticiones http
+
+app.listen(puerto, ()=>{
+    console.log('servidor iniciado en puerto' + puerto)
+})
