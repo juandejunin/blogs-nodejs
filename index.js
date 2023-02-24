48d64ac017a94b9f663b8270eda5525dadf92b50
juan75amieva <juan75amieva@gmail.com>
2023-02-24 17:05:28 +0100

    feat: add architecture

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const cors = require('cors')
 //iniciar la app
 console.log("App de node iniciada")
 
-
-
 conexion()
 
 //crear el servidor
@@ -18,11 +16,30 @@ const puerto = 5000
 app.use(cors())
 
 //convertir el body a un objeto js
-
 app.use(express.json())
 
-//crear el servidor y escuchar peticiones http
+//Rutas
+app.get('/', (req, res) => {
+    console.log('se ejecuto el endpoint probando')
 
-app.listen(puerto, ()=>{
+    return res.status(200).send(
+        `<div>
+        <h1>Blog con nodejs</h1>
+        </div>`
+    )
+})
+app.get('/probando', (req, res) => {
+    console.log('se ejecuto el endpoint probando')
+
+    return res.status(200).json(
+        {
+            curso: "master en chamuyo",
+            autor: "te paese que"
+        }
+    )
+})
+
+//crear el servidor y escuchar peticiones http
+app.listen(puerto, () => {
     console.log('servidor iniciado en puerto' + puerto)
 })
