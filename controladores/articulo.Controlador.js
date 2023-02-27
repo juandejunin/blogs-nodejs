50eb8c9a875e40485d4cac05e64c5f1de519c35b
juan75amieva <juan75amieva@gmail.com>
2023-02-27 08:37:15 +0100

    feat: upload image

--- controladores/articulo.Controlador.js
+++ controladores/articulo.Controlador.js
@@ -1,5 +1,6 @@
-const {validarArticulo} = require ("../helpers/validar")
+const { validarArticulo } = require("../helpers/validar")
 const Articulo = require('../modelos/Articulo')
+const fs = require("fs")
 
 
 
@@ -17,7 +18,6 @@ const crear = (req, res) => {
             status: "error",
             mensaje: "Faltan datos por enviar"
         })
-
     }
 
 
@@ -38,7 +38,6 @@ const crear = (req, res) => {
             return res.status(400).json({
                 status: "error",
                 mensaje: "No se ha guardado el articulo"
-
             })
         }
 
@@ -48,10 +47,7 @@ const crear = (req, res) => {
             articulo: articuloGuardado,
             mensaje: "Articulo creado con exito!!"
         })
-
     })
-
-
 }
 
 const listar = (req, res) => {
@@ -84,7 +80,6 @@ const listarUno = (req, res) => {
     // Recoger un id por url
     let id = req.params.id
 
-
     // Buscar el articulo
     Articulo.findById(id, (error, articulo) => {
         // Si no existe devolver el error
@@ -94,21 +89,18 @@ const listarUno = (req, res) => {
                 mensaje: "No se han encontrado articulos"
             })
         }
-
         // Devolver el resultado
-
         return res.status(200).send({
             status: "success",
             articulo
         })
-
     })
-
 }
 
 const borrar = (req, res) => {
     // Recoger un id por url
     let id = req.params.id
+
 
     //buscar el articulo con la id recibida por parametro url
     Articulo.findByIdAndDelete({ _id: id }, (error, articuloBorrado) => {
@@ -126,8 +118,6 @@ const borrar = (req, res) => {
             mensaje: "El articulo se ha borrado con exito."
         })
     })
-
-
 }
 
 const editar = (req, res) => {
@@ -147,9 +137,7 @@ const editar = (req, res) => {
             status: "error",
             mensaje: "Faltan datos por enviar"
         })
-
     }
-
 
     //Buscar y actualizar articulo
     Articulo.findOneAndUpdate({ _id: id }, req.body, { new: true }, (error, articuloActualizado) => {
@@ -165,20 +153,66 @@ const editar = (req, res) => {
             status: "success",
             articulo: articuloActualizado
         })
-
     })
-
-
 }
 
+const subir = (req, res) => {
 
+    //configurar multer (en el archivo .router)
+
+    //recoger el fichero de imagen subido
+    if (!req.file && !req.files) {
+        return res.status(404).json({
+            status: "error",
+            mensaje: "peticion invalida"
+        })
+    }
+    //nombre del archivo
+    let nombreArchivo = req.file.originalname
+    //extension del archivo
+    let archivo_split = nombreArchivo.split("\.")
+    let archivoExtension = archivo_split[1]
+    //Comprobar que la extension sea la correcta
+    if (archivoExtension != "png" && archivoExtension != "jpg" && archivoExtension != "jpeg" && archivoExtension != "gif" && archivoExtension != "PNG") {
+        //borrar archivo y dar respuesta
+        fs.unlink(req.file.path, (error) => {
+            return res.status(400).json({
+                status: "error",
+                mensaje: "imagen invalida"
+            })
+        })
+    } else {
+        //si todo esta ok, actualizar el archivo
+        // Recoger un id por url
+        let id = req.params.id
+
+        //Buscar y actualizar articulo
+        Articulo.findOneAndUpdate({ _id: id }, ({ imagen: req.file.filename }), { new: true }, (error, articuloActualizado) => {
+            if (error || !articuloActualizado) {
+                return res.status(500).json({
+                    status: "error",
+                    mensaje: "Error al actualizar"
+                })
+            }
+
+            //Devolver respuesta
+            return res.status(200).json({
+                status: "success",
+                articulo: articuloActualizado,
+                fichero: req.file,
+            })
+
+        })
+    }
+}
 
 module.exports = {
     crear,
     listar,
     listarUno,
     borrar,
-    editar
+    editar,
+    subir
 }
 
 
--- rutas/articulo.Ruta.js
+++ rutas/articulo.Ruta.js
@@ -1,6 +1,20 @@
 const {Router} = require("express") 
 const router = Router()
+const multer = require ("multer")
 const ArticuloControlador = require("../controladores/articulo.Controlador")
+// Configurar el almacenamiento, donde se van a guardar mis archivos
+const almacenamiento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './imagenes/articulos/')
+    },
+    filename:(req,file,cb) => {
+        cb(null, "articulo" + Date.now() + file.originalname)
+
+    }
+})
+
+const subidas = multer({storage:almacenamiento})
+
 
 //Rutas utiles
 router.post('/crear', ArticuloControlador.crear)
@@ -8,6 +22,7 @@ router.get('/articulos/:ultimos?', ArticuloControlador.listar)
 router.get('/articulo/:id', ArticuloControlador.listarUno)
 router.delete('/articulo/:id', ArticuloControlador.borrar)
 router.put('/articulo/:id', ArticuloControlador.editar)
+router.post('/subir-imagen/:id',[subidas.single("file0")], ArticuloControlador.subir)
 
 
 
